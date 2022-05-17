07969631b5bec3cb510dc6c37f29575115b34a3c
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-17 13:31:27 -0400

    prepares solution for challenge: return the right-most value in the bottom-most level of a binary tree

---
+++ binary-trees/011.js
@@ -0,0 +1,59 @@
+/*
+
+Bottom Right Value
+
+Write a function, bottomRightValue, that takes in the root of a binary tree. The function should return the right-most value in the bottom-most level of the tree.
+
+You may assume that the input tree is non-empty.
+
+What is a level?
+- It goes across the tree horizontally.
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(10);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     10
+//  / \      \
+// 4   -2     1
+
+const bottomRightValue = (root) => {
+  const queue = [root];
+  let current = null;
+
+  while (queue.length > 0) {
+    current = queue.shift();
+
+    if (current.left !== null) {
+      queue.push(current.left);
+    }
+
+    if (current.right !== null) {
+      queue.push(current.right);
+    }
+  }
+
+  return current.val;
+};
+
+console.log(bottomRightValue(a)); // -> 1
