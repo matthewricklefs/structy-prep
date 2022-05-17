20f687168ab195dd6b5f09a3684f3ed011620ffc
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-17 13:00:40 -0400

    recursive solution for returning the maximum height of a binary tree from the root node to any leaf node

---
+++ binary-trees/010.js
@@ -0,0 +1,46 @@
+/*
+
+How High
+
+Write a function, howHigh, that takes in the root of a binary tree. The function should return a number representing the height of the tree. The height of a binary tree is defined as the maximal number of edges from the root node to any leaf node.
+
+If the tree is empty, return -1;
+
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+const howHigh = (node) => {
+  if (node === null) return -1;
+
+  const leftTreeHeight = howHigh(node.left);
+  const rightTreeHeight = howHigh(node.right);
+  return 1 + Math.max(leftTreeHeight, rightTreeHeight);
+};
+
+console.log(howHigh(a)); // -> 2
