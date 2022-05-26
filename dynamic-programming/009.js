145a8b783ab3102cbe2122d3a7c43606a0faafe6
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-26 15:35:21 -0400

    Counting Change Challenge: func is to return the number of different ways it is possible to make change for the given input integer amount using the input array coins

---
+++ dynamic-programming/009.js
@@ -0,0 +1,41 @@
+/*
+
+Counting Change
+
+Write a function, countingChange, that takes in an amount and an array of coins. The function should return the number of different ways it is possible to make change for the given amount using the coins.
+
+You may reuse a coin as many times as necessary.
+
+For example,
+
+countingChange(4, [1,2,3]) -> 4
+
+There are four different ways to make an amount of 4:
+
+1. 1 + 1 + 1 + 1
+2. 1 + 1 + 2
+3. 1 + 3
+4. 2 + 2
+
+*/
+
+const countingChange = (amount, coins, i = 0, memo = {}) => {
+  const key = amount + ',' + i;
+
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  const coin = coins[i];
+  let total = 0;
+
+  for (let qty = 0; qty * coin <= amount; qty += 1) {
+    const remainder = amount - qty * coin;
+
+    total += countingChange(remainder, coins, i + 1);
+  }
+
+  return total;
+};
+
+console.log(countingChange(4, [1, 2, 3])); // -> 4
+console.log(countingChange(8, [1, 2, 3])); // -> 10
