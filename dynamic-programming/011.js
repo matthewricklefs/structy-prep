dae8e8380044d9cacc66a28d6608161ef7d5c222
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-27 14:00:22 -0400

    mixing brute force recursion and memoization, solves max palin subsequence problem. func to return the length of the longest subsequence of the input string that is also a palindrome

---
+++ dynamic-programming/011.js
@@ -0,0 +1,37 @@
+/*
+
+Max Palin Subsequence
+
+Write a function, maxPalinSubsequence, that takes in a string as an argument. The function should return the length of the longest subsequence of the string that is also a palindrome.
+
+A subsequence of a string can be created by deleting any characters of the string, while maintaining the relative order of characters.
+
+*/
+
+const maxPalinSubsequence = (str, i = 0, j = str.length - 1, memo = {}) => {
+  let key = i + ',' + j;
+  if (key in memo) return memo[key];
+  if (i === j) return 1;
+  if (i > j) return 0;
+
+  if (str[i] === str[j]) {
+    memo[key] = 2 + maxPalinSubsequence(str, i + 1, j - 1, memo);
+  } else {
+    memo[key] = Math.max(
+      maxPalinSubsequence(str, i + 1, j, memo),
+      maxPalinSubsequence(str, i, j - 1, memo)
+    );
+  }
+
+  return memo[key];
+};
+
+console.log(maxPalinSubsequence('luwxult')); // -> 5
+console.log(maxPalinSubsequence('xyzaxxzy')); // -> 6
+console.log(maxPalinSubsequence('z')); // -> 1
+console.log(maxPalinSubsequence('chartreusepugvicefree')); // -> 7
+console.log(
+  maxPalinSubsequence(
+    'enamelpinportlandtildecoldpressedironyflannelsemioticsedisonbulbfashionax)e'
+  )
+); // -> 31
